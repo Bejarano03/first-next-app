158839fb9a1026b2039d9a72e4f811944b1347cf
Bejarano03 <69337575+Bejarano03@users.noreply.github.com>
2024-03-14 21:53:33 -0700

    final commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,36 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <>
+      <header>
+        <h1>Header</h1>
+      </header>
+      <br />
+      <div>
+      <section>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+          porta varius urna. Proin ornare sagittis elit nec faucibus. Quisque in
+          massa quis turpis euismod feugiat. Donec condimentum ex ac ex
+          pellentesque, vitae maximus ipsum faucibus. Curabitur vestibulum nisi
+          laoreet ligula volutpat eleifend.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      </section>
+      <br />
+      <section>
+        <p>
+          Curabitur lobortis mi in ex pellentesque pellentesque. Fusce fermentum
+          urna eu lectus commodo ornare. Curabitur eleifend venenatis tincidunt.
+          Morbi pulvinar aliquam volutpat. Cras dictum ligula ligula, sit amet
+          volutpat diam molestie ac. Vestibulum viverra, lorem quis semper
+          hendrerit, nibh lacus pulvinar odio, non tempus dui ex id nisl.
+          Praesent id dui eu arcu efficitur dignissim a sit amet dolor.
+        </p>
+      </section>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <br />
+      <footer>
+        <h4>Footer</h4>
+      </footer>
+    </>
   );
 }
